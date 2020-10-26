7ce7763274ec5148b696f21e4e89b633af23918e
vasudeveloper001 <vasudeveloper001@gmail.com>
2020-10-26 15:07:16 +0530

    Branch 4: How Java program runs

--- src/com/javatutorial/HelloWorld.java
+++ src/com/javatutorial/HelloWorld.java
@@ -1,7 +1,8 @@
-package first.program;
+package com.javatutorial;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        // write our code here
         System.out.println("Hello World");
     }
 }
